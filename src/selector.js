4c7cb896628ffb7e94fef7d4bbad797b6aafeb4e
Gabriel Henriques <dev.gabrielhenriques@gmail.com>
2020-05-27 08:10:38 -0400

    some cleaning

--- src/selector.js
+++ src/selector.js
@@ -9,16 +9,6 @@ export const getVisibleRestaurants = createSelector(
     if (filter) {
       filter = filter.toLowerCase();
 
-      console.log(
-        "has been called--->",
-        restaurants.filter(
-          (restaurant) =>
-            restaurant.name.toLowerCase().includes(filter) ||
-            restaurant.address.toLowerCase().includes(filter) ||
-            restaurant.area.toLowerCase().includes(filter)
-        )
-      );
-
       return restaurants.filter(
         (restaurant) =>
           restaurant.name.toLowerCase().includes(filter) ||
